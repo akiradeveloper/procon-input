2476e75f450d57f907a7acffcc141452714c007c
akiradeveloper <akiradeveloper@users.noreply.github.com>
2023-01-23 06:46:46 +0000

    deploy: akiradeveloper/procon-input-dev@17055e7c903f2ece51a0cc578fad8c44bd95fef7

--- index-913fd14095d6bcbc.js
+++ index-913fd14095d6bcbc.js
@@ -633,7 +633,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-6b04e089c98f838e_bg.wasm', import.meta.url);
+        input = new URL('index-913fd14095d6bcbc_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
