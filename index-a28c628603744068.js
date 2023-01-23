532770593d495ed790717438ac3b182070df5807
akiradeveloper <akiradeveloper@users.noreply.github.com>
2023-01-23 03:51:18 +0000

    deploy: akiradeveloper/procon-input-dev@555fcce08311faac2629f2890a552da8cf832261

--- index-a28c628603744068.js
+++ index-a28c628603744068.js
@@ -587,12 +587,12 @@ function getImports() {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper257 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 124, __wbg_adapter_16);
+    imports.wbg.__wbindgen_closure_wrapper261 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 125, __wbg_adapter_16);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper609 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 248, __wbg_adapter_19);
+    imports.wbg.__wbindgen_closure_wrapper613 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 249, __wbg_adapter_19);
         return addHeapObject(ret);
     };
 
@@ -630,7 +630,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-e4ff749fc20c16b0_bg.wasm', import.meta.url);
+        input = new URL('index-a28c628603744068_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
